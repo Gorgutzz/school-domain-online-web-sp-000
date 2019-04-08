bb4d0a1620dac05144aeed12f516583c4026c59d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-08 00:22:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,13 @@ class School
     @roster = []
   end
 
-  def add_student(name,grade)
-    if !(@roster[grade])
-      @roster[grade]=[name]
-    else @roster[grade]<<name
-    end
-  end
+  def add_student(name, grade)
+   if @roster[grade] != nil
+     @roster[grade] << name
+   else
+     @roster[grade] = [name]
+   end
+ end
 
 
   def grade(grade)
