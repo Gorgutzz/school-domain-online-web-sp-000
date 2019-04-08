9db6e3a27f02d43cbe16ab321bcce6f39030165d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-08 00:26:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,23 +3,28 @@ class School
 
   attr_accessor :name, :roster
 
-  def initialize (name)
-    @name = name,
-    @roster = {}
+  def initialize(names)
+    @name = name
+    @roster = []
   end
 
   def add_student(name, grade)
-    @roster[grade] = [] if @roster[grade] == nil
-    @roster[grade] << name
-  end
+   if @roster[grade] != nil
+     @roster[grade] << name
+   else
+     @roster[grade] = [name]
+   end
+ end
+
 
   def grade(grade)
     @roster[grade]
   end
 
   def sort
-    @roster.each do |grade, students|
+    @roster.each do|grade, students|
       @roster[grade] = students.sort
     end
   end
+
 end
